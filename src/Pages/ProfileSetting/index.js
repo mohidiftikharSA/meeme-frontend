dfb7c1d3a8edd9e01e439cc7424156d750a7bd3d
saba-ali-02 <sabaali9716@gmail.com>
2023-08-29 23:07:54 +0500

    Delete Account Modals

--- src/Pages/ProfileSetting/index.js
+++ src/Pages/ProfileSetting/index.js
@@ -17,11 +17,12 @@ import Transaction from "Components/Transaction";
 import FAQ from "Components/Faq";
 import RuleList from "Components/RuleList";
 import BilingDetails from "Components/BillingDetails";
-
+import DeleteAccountModal from "Components/DeleteAccountModal";
 
 
 const ProfileSetting = () => {
   const [tabClicked, setTabClicked] = useState(false);
+  const [deleteAccountModalShow, setDeleteAccountShow] = useState(false);
 
   const handleTabClick = () => {
     console.log(tabClicked);
@@ -29,142 +30,147 @@ const ProfileSetting = () => {
   };
 
   return (
-    <section>
-      <Container>
-        <div
-          className="vertical-NavHolder"
-          style={
-            tabClicked
-              ? { maxWidth: "100%" }
-              : { maxWidth: "600px", margin: "auto" }
-          }
-        >
-          <Heading title="Setting" />
-          <Tab.Container id="verticalNav">
-            <Row>
-              <Col sm={tabClicked ? 6 : 12}>
-                <Nav variant="pills" className="flex-column">
-                  <Nav.Item>
-                    <Nav.Link eventKey="account" onClick={handleTabClick}>
-                      <span>
-                        <img src={Profile} alt="Profile" />
-                      </span>
-                      <div className="profileDetails">
-                        <h6 className="mb-1">Account Details</h6>
-                        <p>John Smith</p>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="wallet" onClick={handleTabClick}>
-                      <span>
-                        <img src={bag} alt="bag-icon" />
-                      </span>
-                      <div className="profileDetails">
-                        <h6 className="mb-1">Wallet</h6>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="transaction" onClick={handleTabClick}>
-                      <span>
-                        <img src={documents} alt="document-icon" />
-                      </span>
-                      <div className="profileDetails">
-                        <h6 className="mb-1">Transaction History</h6>
-                        <p>Latest Jan 23 2023</p>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="billing" onClick={handleTabClick}>
-                      <span>
-                        <img src={Wallet} alt="wallet-icon" />
-                      </span>
-                      <div className="profileDetails">
-                        <h6 className="mb-1">Billing Details</h6>
-                        <p>Mastercard ****0123</p>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="notifications" onClick={handleTabClick}>
-                      <span>
-                        <img src={Notification} alt="notification-icon" />
-                      </span>
-                      <div className="profileDetails">
-                        <h6 className="mb-1">Notifications</h6>
-                        <p>Enabled</p>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="support" onClick={handleTabClick}>
-                      <span>
-                        <img src={support} alt="support-icon" />
-                      </span>
-                      <div className="profileDetails">
-                        <h6 className="mb-1">Support</h6>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="faq" onClick={handleTabClick}>
-                      <span>
-                        <img src={Faq} alt="faq-icon" />
-                      </span>
-                      <div className="profileDetails">
-                        <h6 className="mb-1">FAQ</h6>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="rule" onClick={handleTabClick}>
-                      <span>
-                        <img src={file} alt="file-icon" />
-                      </span>
-                      <div className="profileDetails">
-                        <h6 className="mb-1">Rules & Regulation</h6>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="tutorial" onClick={handleTabClick}>
-                      <span>
-                        <img src={msg} alt="msg-icon" />
-                      </span>
-                      <div className="profileDetails">
-                        <h6 className="mb-1">App Tutorial</h6>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="Delete" onClick={handleTabClick}>
-                      <span>
-                        <img src={del} alt="del-icon" />
-                      </span>
-                      <div className="profileDetails">
-                        <h6 className="mb-1">Delete Account</h6>
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                </Nav>
-              </Col>
-              <Col sm={tabClicked ? 6 : 12}>
-                <Tab.Content>
-                  <Tab.Pane eventKey="account"><EditProfile /></Tab.Pane>
-                  <Tab.Pane eventKey="notifications"><NotificationCard/></Tab.Pane>
-                  <Tab.Pane eventKey="faq"><FAQ/></Tab.Pane>
-                  <Tab.Pane eventKey="transaction"><Transaction /></Tab.Pane>
-                  <Tab.Pane eventKey="rule"><RuleList/></Tab.Pane>
-                  <Tab.Pane eventKey="billing"><BilingDetails/></Tab.Pane>
-                </Tab.Content>
-              </Col>
-            </Row>
-          </Tab.Container>
-        </div>
-      </Container>
-    </section>
+  <>
+  <section>
+  <Container>
+    <div
+      className="vertical-NavHolder"
+      style={
+        tabClicked
+          ? { maxWidth: "100%" }
+          : { maxWidth: "600px", margin: "auto" }
+      }
+    >
+      <Heading title="Setting" />
+      <Tab.Container id="verticalNav">
+        <Row>
+          <Col sm={tabClicked ? 6 : 12}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="account" onClick={handleTabClick}>
+                  <span>
+                    <img src={Profile} alt="Profile" />
+                  </span>
+                  <div className="profileDetails">
+                    <h6 className="mb-1">Account Details</h6>
+                    <p>John Smith</p>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="wallet" onClick={handleTabClick}>
+                  <span>
+                    <img src={bag} alt="bag-icon" />
+                  </span>
+                  <div className="profileDetails">
+                    <h6 className="mb-1">Wallet</h6>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="transaction" onClick={handleTabClick}>
+                  <span>
+                    <img src={documents} alt="document-icon" />
+                  </span>
+                  <div className="profileDetails">
+                    <h6 className="mb-1">Transaction History</h6>
+                    <p>Latest Jan 23 2023</p>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="billing" onClick={handleTabClick}>
+                  <span>
+                    <img src={Wallet} alt="wallet-icon" />
+                  </span>
+                  <div className="profileDetails">
+                    <h6 className="mb-1">Billing Details</h6>
+                    <p>Mastercard ****0123</p>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="notifications" onClick={handleTabClick}>
+                  <span>
+                    <img src={Notification} alt="notification-icon" />
+                  </span>
+                  <div className="profileDetails">
+                    <h6 className="mb-1">Notifications</h6>
+                    <p>Enabled</p>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="support" onClick={handleTabClick}>
+                  <span>
+                    <img src={support} alt="support-icon" />
+                  </span>
+                  <div className="profileDetails">
+                    <h6 className="mb-1">Support</h6>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="faq" onClick={handleTabClick}>
+                  <span>
+                    <img src={Faq} alt="faq-icon" />
+                  </span>
+                  <div className="profileDetails">
+                    <h6 className="mb-1">FAQ</h6>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="rule" onClick={handleTabClick}>
+                  <span>
+                    <img src={file} alt="file-icon" />
+                  </span>
+                  <div className="profileDetails">
+                    <h6 className="mb-1">Rules & Regulation</h6>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="tutorial" onClick={handleTabClick}>
+                  <span>
+                    <img src={msg} alt="msg-icon" />
+                  </span>
+                  <div className="profileDetails">
+                    <h6 className="mb-1">App Tutorial</h6>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="Delete" onClick={handleTabClick}>
+                  <span>
+                    <img src={del} alt="del-icon" />
+                  </span>
+                  <div className="profileDetails"  onClick={() => setDeleteAccountShow(true)}>
+                    <h6 className="mb-1">Delete Account</h6>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={tabClicked ? 6 : 12}>
+            <Tab.Content>
+              <Tab.Pane eventKey="account"><EditProfile /></Tab.Pane>
+              <Tab.Pane eventKey="notifications"><NotificationCard/></Tab.Pane>
+              <Tab.Pane eventKey="faq"><FAQ/></Tab.Pane>
+              <Tab.Pane eventKey="transaction"><Transaction /></Tab.Pane>
+              <Tab.Pane eventKey="rule"><RuleList/></Tab.Pane>
+              <Tab.Pane eventKey="billing"><BilingDetails/></Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </div>
+  </Container>
+</section>
+<DeleteAccountModal
+show={deleteAccountModalShow}
+onHide={() => setDeleteAccountShow(false)}/> 
+  </>
   );
 };
 
