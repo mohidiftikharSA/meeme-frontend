a407518c073d010447648e1287cf7eecacbcf141
saba-ali-02 <sabaali9716@gmail.com>
2023-09-03 16:37:57 +0500

    edit judgeModal

---
+++ src/Components/EarnCoinsModal/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Button, Modal } from 'react-bootstrap'
+import classes from "./index.module.scss";
+import { LiaTimesSolid } from 'react-icons/lia';
+
+const EarnCoinsModal= (props) => {
+    return (
+        <Modal className={classes.infoModal}
+            {...props}
+            size="sm"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header className='justify-content-end'>
+                <span className={classes.closeBtn} onClick={props.onHide}><LiaTimesSolid /></span>
+            </Modal.Header>
+            <Modal.Body className='m-0'>
+            <h4 className='title'>Judge</h4>
+            <p className='text'>To qualify for  coin rewards, you need to judge 100 memes per day</p>
+            <Button onClick={props.onHide} className='reset-btn px-4 mt-2'>Agree</Button>
+            </Modal.Body>
+        </Modal>
+    )
+}
+
+export default EarnCoinsModal
--- src/Components/Heading/index.js
+++ src/Components/Heading/index.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosArrowBack } from 'react-icons/io';
 import classes from "./index.module.scss"
 import { useNavigate } from 'react-router-dom';
-const Heading = ({title, judge}) => {
+import CongratsModal from 'Components/CongratsModal';
+const Heading = ({ title, judge }) => {
+  const [modalShow, setModalShow] = useState(false);
   const navigate = useNavigate();
-    const backPage = () => {
-      navigate(`/home`);
-    };
+  const backPage = () => {
+    navigate(`/home`);
+  };
 
   return (
-    <div className={`${classes.heading} ${judge&& `${classes.heading} d-flex align-items-center justify-content-between`} `} onClick={backPage}>
-        <h5><IoIosArrowBack/>{title}</h5>
-      {
-        judge&&
-        <div className={classes.memeNo}><span className='text-light'>40</span>/100</div>
-      }
-    </div>
+    <>
+      <div className={`${classes.heading} ${judge && `${classes.heading} d-flex align-items-center justify-content-between`} `} onClick={backPage}>
+        <h5><IoIosArrowBack />{title}</h5>
+        {
+          judge &&
+          <div className={classes.memeNo}><span className='text-light' onClick={() => setModalShow(true)}>40</span>/100</div>
+        }
+      </div>
+      <CongratsModal
+        show={modalShow}
+        onHide={() => setModalShow(false)} />
+    </>
   )
 }
 
