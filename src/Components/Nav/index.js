9c66d8bea9901a2a5d44919d64ded2fdeb826518
saba-ali-02 <sabaali9716@gmail.com>
2023-09-06 06:46:37 -0700

    Trello Fixes

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -11,37 +11,37 @@ import trophies from '../../Images/trophy (1) 1.png'
 
 const Navigation = ({ header, footer }) => {
   return (
-    <Navbar expand="lg" className={header? "navBar bg-body-tertiary" : "navBar aside bg-body-tertiary py-5"} >
-    {header? 
-    <>
-    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="/home"><img src={home} alt="icon"></img> Home</Nav.Link>
-        <Nav.Link href="/explore"> <img src={expore} alt="icon"></img>Explore</Nav.Link>
-        <Nav.Link href="/tornament"><img src={trophy} alt="icon"></img>Tournament</Nav.Link>
-        <Nav.Link href="/BuyCoin"><img src={Buy} alt="icon"></img>Store</Nav.Link>
-      </Nav>
-    </Navbar.Collapse> 
-    </>
-    :
-    <Nav className="me-auto flex-column w-100">
-    <Nav.Link href="/home" className="mb-3 active">
-    <img src={home} alt="icon"></img> Home
-    </Nav.Link>
-    <Nav.Link href="/tornament" className="btn btn-primary btn-bg trophy mb-3">
-    <img src={trophies} alt="icon"></img>Tournament
-    </Nav.Link>
-    <Nav.Link className="btn btn-primary btn-bg store mb-3">
-    <img src={judge} alt="icon"></img>Judge
-    </Nav.Link>
-    <Nav.Link className="btn btn-primary btn-bg judge mb-3">
-    <img src={shop} alt="icon"></img>Store
-    </Nav.Link>
-  </Nav>
-}
-  
-  </Navbar>
+    <Navbar expand="lg" className={header ? "navBar bg-body-tertiary" : "navBar aside bg-body-tertiary py-5"} >
+      {header ?
+        <>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/home"><img src={home} alt="icon"></img> Home</Nav.Link>
+              <Nav.Link href="/explore"> <img src={expore} alt="icon"></img>Explore</Nav.Link>
+              <Nav.Link href="/tornament"><img src={trophy} alt="icon"></img>Tournament</Nav.Link>
+              <Nav.Link href="/BuyCoin"><img src={Buy} alt="icon"></img>Store</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </>
+        :
+        <Nav className="me-auto flex-column w-100">
+          <Nav.Link href="/home" className="mb-3 active">
+            <img src={home} alt="icon"></img> Home
+          </Nav.Link>
+          <Nav.Link href="/tornament" className="btn btn-primary btn-bg trophy mb-3">
+            <img src={trophies} alt="icon"></img>Tournament
+          </Nav.Link>
+          <Nav.Link className="btn btn-primary btn-bg store mb-3">
+            <img src={judge} alt="icon"></img>Judge
+          </Nav.Link>
+          <Nav.Link className="btn btn-primary btn-bg judge mb-3">
+            <img src={shop} alt="icon"></img>Store
+          </Nav.Link>
+        </Nav>
+      }
+
+    </Navbar>
   );
 };
 
