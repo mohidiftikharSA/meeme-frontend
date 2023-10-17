ee60681619eb64bc13441bfd229447197d9dc694
“fahad4787” <nfahad4787@gmail.com>
2023-10-17 12:23:54 +0500

    viewpost added

--- src/Components/Memes/index.js
+++ src/Components/Memes/index.js
@@ -1,33 +1,63 @@
-import React from 'react';
-import classes from './index.module.scss';
-import dummyUser from '../../Images/user-dummy.png';
+import React, { useState } from "react";
+import classes from "./index.module.scss";
+import dummyUser from "../../Images/user-dummy.png";
+import ViewPost from "Components/ViewPost";
 
 const MemesDetails = ({ newMemesData, explore }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPostId, setSelectedPostId] = useState(null);
+
+  const openModal = (postId) => {
+    setSelectedPostId(postId);
+    setIsModalOpen(true);
+    console.log(postId);
+  };
+
+  const closeModal = () => {
+    setSelectedPostId(null);
+    setIsModalOpen(false);
+  };
   // Function to determine if the item should be displayed as an image
   function isImage(item) {
     // Check if the 'post_type' field is defined and indicates that the item is an image
-    return item.post_type && item.post_type.startsWith('image/');
+    return item.post_type && item.post_type.startsWith("image/");
   }
 
   return (
-    <div className={`${classes.flexBox} ${explore ? `${classes.exploreBox}` : ''}`}>
-      {newMemesData.map((item, ind) => (
-        <div key={ind} className={classes.imgBox}>
-          {isImage(item) ? (
-            <img src={item.compress_image} alt="img" />
-          ) : (
-            <video controls>
-              <source src={item.compress_image} type={item.post_type} />
-              Your browser does not support the video tag.
-            </video>
-          )}
-          <div className={classes.profileDetail}>
-            <img src={item.user_image || dummyUser} alt='icon' />
-            <span>{item.username}</span>
+    <>
+      <div
+        className={`${classes.flexBox} ${
+          explore ? `${classes.exploreBox}` : ""
+        }`}
+      >
+        {newMemesData.map((item, ind) => (
+          <div
+            key={ind}
+            className={classes.imgBox}
+            onClick={() => openModal(item.id)}
+          >
+            {isImage(item) ? (
+              <img src={item.compress_image} alt="img" />
+            ) : (
+              <video controls>
+                <source src={item.compress_image} type={item.post_type} />
+                Your browser does not support the video tag.
+              </video>
+            )}
+            <div className={classes.profileDetail}>
+              <img src={item.user_image || dummyUser} alt="icon" />
+              <span>{item.username}</span>x
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <ViewPost
+        onHide={closeModal}
+        show={isModalOpen}
+        selectedPostId={selectedPostId}
+        postData={newMemesData}
+      />
+    </>
   );
 };
 
