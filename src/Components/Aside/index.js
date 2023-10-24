0ae92e72b306f999e83f5c00b2c543b24626e405
saba-ali-02 <sabaali9716@gmail.com>
2023-10-24 06:43:50 -0700

    setting to settings

--- src/Components/Aside/index.js
+++ src/Components/Aside/index.js
@@ -67,7 +67,7 @@ export const Aside = ({isActive, toggleActive}) => {
               }}
             >
               <img src={setting} style={{ marginRight: "8px" }} alt="Profile" />{" "}
-              Setting
+              Settings
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
