aca0135094448020453486ace3b099cf38269841
Haseeb71 <71abdulhaseeb71@gmail.com>
2023-10-24 17:58:55 +0500

    Authentication of routes

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -6,64 +6,69 @@ import routes from "./routes";
 import { useSelector } from "react-redux";
 
 const ClientRoutes = () => {
-    const { accessToken } = useSelector((state) => state.auth);
-    const getElementControl = (path, authenticated, component, layout) => {
-        if (!authenticated) {
-          if (accessToken) {
-            if (path === '/login' ||
-              path === '/forgetpassword' ||
-              path === '/forgetPassword' ||
-              path === '/restsetPassword' ||
-              path === '/signup' ||
-              path === '/emailVerification' ||
-              path === '/emailVerification'
-            ) {
-              return <Navigate to="/home" />
-            }
-          }
-          return <ElementControl Component={component} Layout={layout} />;
+  var { accessToken } = useSelector((state) => state.auth);
+  if (!accessToken) {
+    accessToken = localStorage.getItem("accessToken")
+  }
+  console.log("Access Token of Routes  =", accessToken);
+
+  const getElementControl = (path, authenticated, component, layout) => {
+    if (!authenticated) {
+      if (accessToken) {
+        if (path === '/login' ||
+          path === '/forgetpassword' ||
+          path === '/forgetPassword' ||
+          path === '/restsetPassword' ||
+          path === '/signup' ||
+          path === '/emailVerification' ||
+          path === '/emailVerification'
+        ) {
+          return <Navigate to="/home" />
         }
-        if (authenticated) {
-          if (accessToken) {
-            return <ElementControl Component={component} Layout={layout} />;
-          } else {
-            return <Navigate to="/login" />;
-          }
-        }
-      };
-    return (
-        <BrowserRouter>
-            <Routes>
-                {routes.map((route, index) => {
-                    const { path, subRoutes, component, layout, authenticated} = route;
-                    if (subRoutes && subRoutes.length > 0) {
-                        return (
-                            <Route key={`route_${index}`}>
-                                {subRoutes.map((subRoute, subIndex) => {
-                                    return (
-                                        <Route
-                                            key={`subroute_${subIndex}`}
-                                            exact
-                                            path={`${route.path}${subRoute.path}`}
-                                            element={getElementControl(path, authenticated, subRoute.component, layout)}
-                                        />
-                                    );
-                                })}
-                            </Route>
-                        );
-                    }
-                    return (
-                        <Route
-                            key={`route_${index}`}
-                            exact
-                            path={path}
-                            element={getElementControl(path, authenticated, component, layout)}
-                        />
-                    );
+      }
+      return <ElementControl Component={component} Layout={layout} />;
+    }
+    if (authenticated) {
+      if (accessToken) {
+        return <ElementControl Component={component} Layout={layout} />;
+      } else {
+        return <Navigate to="/login" />;
+      }
+    }
+  };
+  return (
+    <BrowserRouter>
+      <Routes>
+        {routes.map((route, index) => {
+          const { path, subRoutes, component, layout, authenticated } = route;
+          if (subRoutes && subRoutes.length > 0) {
+            return (
+              <Route key={`route_${index}`}>
+                {subRoutes.map((subRoute, subIndex) => {
+                  return (
+                    <Route
+                      key={`subroute_${subIndex}`}
+                      exact
+                      path={`${route.path}${subRoute.path}`}
+                      element={getElementControl(path, authenticated, subRoute.component, layout)}
+                    />
+                  );
                 })}
-            </Routes>
-        </BrowserRouter>
-    );
+              </Route>
+            );
+          }
+          return (
+            <Route
+              key={`route_${index}`}
+              exact
+              path={path}
+              element={getElementControl(path, authenticated, component, layout)}
+            />
+          );
+        })}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default ClientRoutes;
