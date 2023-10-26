00970a0f89d46d12c9b35f77e11a12f8d0296a6f
“fahad4787” <nfahad4787@gmail.com>
2023-10-26 11:32:38 +0500

    fies

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -7,13 +7,12 @@ import Buy from "../../Images/Buy.png";
 import judge from "../../Images/judges.png";
 import shop from "../../Images/shop.png";
 import trophies from "../../Images/trophy (1) 1.png";
-import { FaBars } from 'react-icons/fa';
-import { IoClose } from 'react-icons/io5';
+import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import coin from "../../Images/coin.png";
-import profile from "../../Images/profile1.png"
-import classes from "./index.module.scss"
-
+import profile from "../../Images/profile1.png";
+import classes from "./index.module.scss";
 
 const Navigation = ({ header, footer }) => {
   const [isActive, setIsActive] = useState(false);
@@ -21,59 +20,80 @@ const Navigation = ({ header, footer }) => {
   const toggleActive = () => {
     setIsActive(!isActive);
   };
+  const addActiveClass = (match, location) => {
+    return match ? "active" : "";
+  };
+
   return (
     <Navbar
       expand="lg"
       className={
         header
           ? "navBar bg-body-tertiary"
-          : "navBar aside bg-body-tertiary py-lg-5 py-3 "
+          : "navBar aside bg-body-tertiary pt-lg-5 pb-lg-3 py-3 "
       }
     >
       {header ? (
         <>
           {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-          <span onClick={toggleActive} className={`${isActive ? "d-none" : "d-block"}`}><FaBars className="custom-icon1" /></span>
+          <span
+            onClick={toggleActive}
+            className={`${isActive ? "d-none" : "d-block"}`}
+          >
+            <FaBars className="custom-icon1" />
+          </span>
           <Navbar className={`responsive-nav ${isActive ? "active" : ""}`}>
-          <span onClick={toggleActive} className={`${isActive ? "d-block" : "d-none"}`}><IoClose className="custom-icon2" /></span>
+            <span
+              onClick={toggleActive}
+              className={`${isActive ? "d-block" : "d-none"}`}
+            >
+              <IoClose className="custom-icon2" />
+            </span>
 
             <Nav className="me-auto flex-lg-row flex-column ">
-              <Nav.Link href="/home">
-                <img src={home} alt="icon"></img> Home
+              <Nav.Link
+                className={`nav-link ${addActiveClass("/home")}`}
+                as={Link}
+                to="/home"
+              >
+                <img src={home} alt="icon" />
+                Home
               </Nav.Link>
-              <Nav.Link href="/explore">
-                {" "}
+              <Nav.Link as={Link} to="/explore"
+              className={`nav-link ${addActiveClass("/explore")}`}
+              >
+
                 <img className="nav-icon" src={expore} alt="icon"></img>Explore
               </Nav.Link>
-              <Nav.Link href="/tornament">
+              <Nav.Link as={Link} to="/tornament">
                 <img src={trophy} alt="icon"></img>Tournament
               </Nav.Link>
-              <Nav.Link href="/BuyCoin">
+              <Nav.Link as={Link} to="/BuyCoin">
                 <img src={Buy} alt="icon"></img>Store
               </Nav.Link>
               <ButtonGroup className="align-items-center" id="profile-btn">
-              <Link to={"/Purchase"} className={`btn ${classes.iconBtn}`}>
-                <span className={classes.icon}>
-                <i className="fas fa-plus"></i>
-                </span>
-                <span className={classes.text}>7412</span>
-                <img src={coin} alt="icon" />
-              </Link>
+                <Link to={"/Purchase"} className={`btn ${classes.iconBtn}`}>
+                  <span className={classes.icon}>
+                    <i className="fas fa-plus"></i>
+                  </span>
+                  <span className={classes.text}>7412</span>
+                  <img src={coin} alt="icon" />
+                </Link>
 
-              <Link to={"/profile"} className={`btn ${classes.profileBtn}`}>
-                <img src={profile} alt="icon" />
-              </Link>
-            </ButtonGroup>
+                <Link to={"/profile"} className={`btn ${classes.profileBtn}`}>
+                  <img src={profile} alt="icon" />
+                </Link>
+              </ButtonGroup>
             </Nav>
           </Navbar>
         </>
       ) : (
         <Nav className="me-auto flex-column w-100 nav-links">
-          <Nav.Link href="/home" className="mb-3 active home-icon">
-            <img  src={home} alt="icon"></img> Home
+          <Nav.Link to="/home" className="mb-3 active home-icon">
+            <img src={home} alt="icon"></img> Home
           </Nav.Link>
           <Nav.Link
-            href="/tornament"
+            to="/tornament"
             className="btn btn-primary btn-bg trophy mb-3"
           >
             <img src={trophies} alt="icon"></img>Tournament
--- src/Pages/LandingPage/index.js
+++ src/Pages/LandingPage/index.js
@@ -13,18 +13,18 @@ const LandingPage = () => {
   };
   return (
     <>
-      <Row>
-        <Col xl={3} md={4} className={`pe-0 aside-responsive ${isActive ? "active" : ""}`}>
+      <Row className="main-page">
+        <Col xl={3} md={4} className={`pe-0 aside-responsive page-sides ${isActive ? "active" : ""}`}>
           <Aside isActive={isActive} toggleActive={toggleActive} />         
         </Col>
-        <Col xl={6} md={8} className={`p-lg-0 content-responsive ${isActive ? "active" : ""}`}>
+        <Col xl={6} md={8} className={`p-lg-0 content-responsive center-side ${isActive ? "active" : ""}`}>
           <section>
             <Container>
               <TabDetails first={"following"} main />
             </Container>
           </section>
         </Col>
-        <Col xl={3} className="d-xl-block d-none">
+        <Col xl={3} className="d-xl-block d-none page-sides">
           <AccordianData following />
         </Col>
       </Row>
