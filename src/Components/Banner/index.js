c34de859acc836e13c78fec4c000abbde3453cd4
saba-ali-02 <sabaali9716@gmail.com>
2023-11-14 18:53:28 -1200

    chatbox recheck

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -1,3 +1,136 @@
+// import React, { useCallback, useEffect, useState } from "react";
+// import classes from "./index.module.scss";
+// import userimg from "../../Images/avatar.jpg";
+// import { Button } from "react-bootstrap";
+// import setting from "../../Images/SettingIcon.png";
+// import edit from "../../Images/editUser.png";
+// import { useNavigate } from "react-router-dom";
+// import FollowModal from "Components/FollowModal";
+// import FollowerAPIs from '../../APIs/followers';
+// import { toast } from "react-toastify";
+
+
+// const Banner = ({ other, profile }) => {
+//   const [show, setShow] = useState(false);
+//   const [follwers, setfollowrshow] = useState(false);
+//   const navigate = useNavigate();
+//   const [isFollowing, setIsFollowing] = useState(false);
+//   const [followingList , setFollowingList ] = useState([]);
+//   const [followersList , setFollowersList ] = useState([]);
+
+//   const nextPage = () => {
+//     navigate(`/CustomizeProfile`);
+//   };
+
+//   useEffect(() => {
+
+//     if (profile?.follower_added) {
+//       setIsFollowing(true);
+//     }
+
+//     getFollowersAndFollowings();
+
+//   }, [profile])
+
+//   const getFollowersAndFollowings = useCallback( async()=>{
+//     const followings = await FollowerAPIs.followingList();
+//     if(followings){
+//       setFollowingList(followings?.data?.followings);
+//     }
+
+//     const followers = await FollowerAPIs.followersList();
+//     if(followers){
+//       setFollowersList(followers?.data?.followers)
+//     }
+
+//   },[profile])
+
+
+
+//   const followOrUnfollow = async (action) => {
+//     console.log("Follow Profile = ", profile);
+//     if (action === 'follow') {
+//       const follow = await FollowerAPIs.sendFollowRequest({ follower_user_id: profile?.user?.id });
+//       if (follow) {
+//         setIsFollowing(true);
+//         console.log("Follow Successfull =", follow);
+//         toast.success('Followed Successfully', {
+//           position: "top-right",
+//           autoClose: 2000,
+//         });
+//       }
+//     } else if (action === 'unfollow') {
+//       const unfollow = await FollowerAPIs.unfollowUser({ follower_user_id: profile?.user?.id })
+//       if (unfollow) {
+//         setIsFollowing(false);
+//         console.log("unfollow Successfull =", unfollow);
+//         toast.success('Unfollow Successfully', {
+//           position: "top-right",
+//           autoClose: 2000,
+//         });
+//       }
+//     }
+
+//   }
+
+//   return (
+//     <>
+//       <div className={`banner pb-4 px-2 ${other && `banner pb-4 otherBanner`}`}>
+//         <div className="sectionHolder" style={{ maxWidth: "350px" }}>
+//           <div className={classes.profileDetail}>
+//             <div className={classes.profileDetails}>
+//               <img src={profile?.user_image || userimg} alt="" />
+//               <h4 >{profile?.user?.username}</h4>
+//             </div>
+//             {
+//               other &&
+//               <div className={classes.btnBox}>
+//                 {isFollowing ? <Button onClick={() => { followOrUnfollow('unfollow') }} >UnFollow</Button> : <Button onClick={() => { followOrUnfollow('follow') }} >Follow</Button>}
+//                 <Button>Message</Button>
+//               </div>
+//             }
+//             <ul className={classes.userInfoBox}>
+//               <li>
+//                 <h5 >
+//                   {profile?.all_post_count} <span>Posts</span>
+//                 </h5>
+//               </li>
+//               <li>
+//                 <h5 onClick={() => { setfollowrshow(true) }}>
+//                   {profile?.followers} <span>Followers</span>
+//                 </h5>
+//               </li>
+//               <li>
+//                 <h5 onClick={() => { setShow(true) }}>
+//                   {profile?.following} <span>Followings</span>
+//                 </h5>
+//               </li>
+//             </ul>
+//             <h6 className="mb-4 text-center">
+//               {profile?.user?.bio}
+//             </h6>
+//             <div className={classes.btnGroup}>
+//               <Button onClick={nextPage}>
+//                 <img src={edit} alt="img" />
+//               </Button>
+//               <Button>
+//                 <img src={setting} alt="img" />
+//               </Button>
+//             </div>
+//           </div>
+//         </div>
+
+//       </div>
+//       <FollowModal following show={show} followingList={followingList} onHide={() => setShow(false)} />
+//       <FollowModal followers show={follwers} followersList={followersList} onHide={() => setfollowrshow(false)} />
+//     </>
+//   );
+// };
+
+// export default Banner;
+
+
+
 
 import React, { useCallback, useEffect, useState } from "react";
 import classes from "./index.module.scss";
@@ -9,7 +142,7 @@ import { useNavigate } from "react-router-dom";
 import FollowModal from "Components/FollowModal";
 import FollowerAPIs from '../../APIs/followers';
 import { toast } from "react-toastify";
-import ChatPopup from "Components/ChatPopup"; 
+import ChatPopup from "Components/ChatPopup";
 
 const Banner = ({ other, profile }) => {
   const [show, setShow] = useState(false);
@@ -96,7 +229,21 @@ const Banner = ({ other, profile }) => {
               </div>
             )}
             <ul className={classes.userInfoBox}>
-              {/* ... existing code ... */}
+              <li>
+                <h5 >
+                  {profile?.all_post_count} <span>Posts</span>
+                </h5>
+              </li>
+              <li>
+                <h5 onClick={() => { setfollowrshow(true) }}>
+                  {profile?.followers} <span>Followers</span>
+                </h5>
+              </li>
+              <li>
+                <h5 onClick={() => { setShow(true) }}>
+                  {profile?.following} <span>Followings</span>
+                </h5>
+              </li>
             </ul>
             <h6 className="mb-4 text-center">
               {profile?.user?.bio}
--- src/Components/ChatPopup/index.js
+++ src/Components/ChatPopup/index.js
@@ -1,3 +1,137 @@
+// import React, { useEffect, useState } from "react";
+// import { Dropdown, Form } from "react-bootstrap";
+// import { LiaTimesSolid } from "react-icons/lia";
+// import user1 from "../../Images/chatuser.png";
+// import user2 from "../../Images/profile1.png";
+// import { FiSmile } from 'react-icons/fi';
+// import { CgAttachment } from 'react-icons/cg';
+// import { BsFillChatDotsFill } from 'react-icons/bs';
+
+
+// const ChatPopup = () => {
+//   const [messages, setMessages] = useState([]);
+//   const [inputText, setInputText] = useState("");
+//   const [currentUser, setCurrentUser] = useState("User1"); // Change user names as needed
+//   const [isDropdownOpen, setIsDropdownOpen] = useState(false); // State to manage dropdown visibility
+//   const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 992);
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setIsWideScreen(window.innerWidth >= 768);
+//     };
+
+//     window.addEventListener("resize", handleResize);
+
+//     return () => {
+//       window.removeEventListener("resize", handleResize);
+//     };
+//   }, []);
+
+//   // Dummy message from other user
+//   const dummyMessage = [
+//     { text: "Hi Astro", user: "Randy" },
+//     { text: "Can you send me a meme" },
+//   ];
+
+//   const handleInputChange = (e) => {
+//     setInputText(e.target.value);
+//   };
+
+//   const sendMessage = () => {
+//     if (inputText.trim() === "") return;
+
+//     const newMessage = {
+//       text: inputText,
+//       user: currentUser,
+//     };
+
+//     setMessages([...messages, newMessage]);
+//     setInputText("");
+//   };
+
+//   const handleInputKeyPress = (e) => {
+//     if (e.key === "Enter") {
+//       sendMessage();
+//     }
+//   };
+
+//   const toggleDropdown = () => {
+//     setIsDropdownOpen(!isDropdownOpen);
+//   };
+
+//   return (
+//     <Dropdown className={`chatDropDown ${isDropdownOpen ? 'show' : ''}`} show={isDropdownOpen} onToggle={toggleDropdown}>
+//        <Dropdown.Toggle variant="success" id="dropdown-basic">
+//          {isWideScreen ?<><span className="status d-inline-block me-2"></span> Chat (4 Active)</> : <BsFillChatDotsFill size={'18px'}/>}
+//       </Dropdown.Toggle>
+//       <Dropdown.Menu onClick={(e) => e.stopPropagation()}>
+//         <div className="chat">
+//           <div className="chat-window">
+//             <div className="header">
+//               <span className="status d-inline-block mx-2"></span>Randy Mark
+//               <span className="cancel-icon" onClick={toggleDropdown} style={{cursor:"pointer"}}>
+//                 <LiaTimesSolid />
+//               </span>
+//             </div>
+//             <div className="messages">
+//               <div className="reciverBox">
+//                 <div className="userImg">
+//                   <img src={user1} alt="img" />
+//                 </div>
+//                 {dummyMessage.map((item, ind) => {
+//                   return (
+//                     <div key={ind} className="message received">
+//                       <div className="messageBox">
+//                         <div className="message-user">{item.user}</div>
+//                         <div className="message-text">{item.text}</div>
+//                       </div>
+//                     </div>
+//                   );
+//                 })}
+//               </div>
+//               <div className="senderBox">
+//                 {messages.map((message, index) => (
+//                   <div
+//                     key={index}
+//                     className={`message ${
+//                       message.user === currentUser ? "sent" : "received"
+//                     }`}
+//                   >
+//                     <div className="userImg">
+//                       {message.user === currentUser && (
+//                         <img src={user2} alt="img" />
+//                       )}
+//                     </div>
+//                     <div className="messageBox">
+//                       <div className="message-text ">{message.text}</div>
+//                     </div>
+//                   </div>
+//                 ))}
+//               </div>
+//             </div>
+//             <div className="sendBox">
+//               <Form.Control
+//                 type="text"
+//                 placeholder="Write a comment…"
+//                 value={inputText}
+//                 onChange={handleInputChange}
+//                 onKeyPress={handleInputKeyPress}
+//               />
+//               <div className={'iconBox'}>
+//                 <FiSmile/>
+//                 <CgAttachment/>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </Dropdown.Menu>
+//     </Dropdown>
+//   );
+// };
+
+// export default ChatPopup;
+
+
 
 import React, { useEffect, useState } from "react";
 import { Dropdown, Form } from "react-bootstrap";
