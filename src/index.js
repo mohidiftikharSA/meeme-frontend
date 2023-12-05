69eac2019394aa788a0a0a22ce8e1b84fdb9c74d
usman <usman@softwarealliance.dk>
2023-12-05 12:51:46 +0500

    enter key press in chat message and some console errors fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "Containers/App";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "../src/Redux/store";
-import { ToastContainer } from "react-toastify";
+import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
+import {store, persistor} from "../src/Redux/store";
+import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import actionCable from 'actioncable'
 import 'react-loading-skeleton/dist/skeleton.css'
@@ -14,13 +14,11 @@ CableApp.cable = actionCable.createConsumer('ws://staging.memee.app/cable')
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
+root.render(<Provider store={store}>
     <PersistGate persistor={persistor}>
-     {/* <React.StrictMode>*/}
-        <App />
-        <ToastContainer position="top-right" autoClose={2000} />
-      {/*</React.StrictMode>*/}
+        <React.StrictMode>
+            <App/>
+            <ToastContainer position="top-right" autoClose={2000}/>
+        </React.StrictMode>
     </PersistGate>
-  </Provider>
-);
+</Provider>);
