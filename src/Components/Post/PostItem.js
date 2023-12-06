9efa51d29a9b8e4b59704c8f2fdd767c2d23a269
saba-ali-02 <sabaali9716@gmail.com>
2023-12-05 17:44:45 -1200

    checkbox

--- src/Components/Post/PostItem.js
+++ src/Components/Post/PostItem.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {Dropdown} from "react-bootstrap";
-import {timeAgo, formatNumber} from "../../Helper/Converters";
-import {useNavigate} from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
+import { timeAgo, formatNumber } from "../../Helper/Converters";
+import { useNavigate } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
 import classes from "./index.module.scss";
 import like from "../../Images/like.svg";
@@ -11,27 +11,28 @@ import user2 from "../../Images/avatar.jpg";
 import avatar from "../../Images/avatar.jpg";
 
 const PostItem = ({
-                      item,
-                      ind,
-                      likePost,
-                      downloadMedia,
-                      redirectToOtherProfile,
-                      openModal,
-                      copyToClipboard,
-                      handleImageLoad,
-                      handleImageError,
-                      imagesLoaded,
-                      comment
-                  }) => {
+    item,
+    ind,
+    likePost,
+    downloadMedia,
+    redirectToOtherProfile,
+    openModal,
+    copyToClipboard,
+    handleImageLoad,
+    handleImageError,
+    imagesLoaded,
+    comment
+}) => {
     const navigate = useNavigate();
 
-    return (<div className={classes.postWrapper} key={ind}>
+    return (
+    <div className={classes.postWrapper} key={ind}>
         <div className={classes.postHeader}>
             <div className={classes.profile}>
                 <div className={classes.imgBox}>
                     {/* update condition to profile post */}
                     <img src={item.user_image || (avatar.avatar || user2)}
-                         alt="user"/>
+                        alt="user" />
                 </div>
                 <div className={classes.userDetail}>
                     <h6 onClick={() => {
@@ -62,22 +63,22 @@ const PostItem = ({
             {item.post.tag_list && <p className={classes.tags}>{item.post.tag_list}</p>}
         </div>
         <div className={`${classes.imgBox} memeImg mb-3`}
-             onClick={() => openModal(item.post.id)}>
+            onClick={() => openModal(item.post.id)}>
             {item.post_type === "image/jpeg" || item.post_type === "image/png" ? (<div>
-                <div style={{display: imagesLoaded[ind] ? 'none' : 'block'}}>
+                <div style={{ display: imagesLoaded[ind] ? 'none' : 'block' }}>
                     <Skeleton
                         height={300} width="100%"
                         style={{
                             marginTop: '10px', borderRadius: '20px'
-                        }}/>
+                        }} />
                 </div>
                 <img
-                    style={{display: imagesLoaded[ind] ? 'block' : 'none'}}
+                    style={{ display: imagesLoaded[ind] ? 'block' : 'none' }}
                     onLoad={() => handleImageLoad(ind)}
                     onError={() => handleImageError(ind)}
-                    src={item.compress_image} alt="img"/>
+                    src={item.compress_image} alt="img" />
             </div>) : (<video width="100%" height="auto" controls>
-                <source src={item.post_image} type="video/mp4"/>
+                <source src={item.post_image} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>)}
         </div>
@@ -86,21 +87,21 @@ const PostItem = ({
             <li key={item.post.id}
                 className={`item ${item.liked_by_current_user ? `${classes.active}` : ''}`}
                 onClick={() => likePost(item.post.id)}>
-                <img src={like} alt="img"/>
+                <img src={like} alt="img" />
                 <span> {formatNumber(item.post_likes)}</span>
             </li>
             {comment ? <li>
-                <img src={message} alt="img"/>
+                <img src={message} alt="img" />
                 <span>{formatNumber(item.post_comments_count)}</span>
             </li> : <li onClick={() => openModal(item.post.id)}>
-                <img src={message} alt="img"/>
+                <img src={message} alt="img" />
                 <span>{formatNumber(item.post_comments_count)}</span>
             </li>}
 
             <li>
                 <img onClick={() => {
                     copyToClipboard(item.compress_image)
-                }} src={send} alt="img"/>
+                }} src={send} alt="img" />
                 {/* <span>{formatNumber(item.post.share_count)}</span> */}
             </li>
         </ul>
--- src/Components/Post/index.js
+++ src/Components/Post/index.js
@@ -7,7 +7,7 @@ import SkeletonPostsLoading from '../Loader/SkeletonPostsLoading'
 import PostItem from "./PostItem";
 import avatar from "../../Images/avatar.jpg";
 
-const Posts = ({postData, comment, isLoading}) => {
+const Posts = ({postData, comment, isLoading,disable}) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedPostId, setSelectedPostId] = useState(null);
     const [followingData, setFollowingData] = useState([]);
@@ -123,6 +123,7 @@ const Posts = ({postData, comment, isLoading}) => {
             handleImageError={handleImageError}
             imagesLoaded={imagesLoaded}
             comment={comment}
+            disable
         />)}
 
         <ViewPost onHide={closeModal} show={isModalOpen} selectedPostId={selectedPostId} postData={postData}
