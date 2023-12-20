88458f69145e60faa168746de9b2d2bdd0def1c5
usman <usman@softwarealliance.dk>
2023-12-20 18:42:23 +0500

    changes in explore page tags

--- src/Components/AccordainBadge/index.js
+++ src/Components/AccordainBadge/index.js
@@ -1,43 +1,44 @@
-import React, { useState } from "react";
-import { Badge } from "react-bootstrap";
+import React, {useState} from "react";
+import {Badge} from "react-bootstrap";
 import classes from "./index.module.scss"
 
-const AccordianBadge = ({ data, expolore , selectedTagsString}) => {
-  const [activeStates, setActiveStates] = useState(Array(data.length).fill(false));
-  const [selectedTags, setSelectedTags] = useState('');
+const AccordianBadge = ({data, expolore, onTagSelect}) => {
+    const [activeStates, setActiveStates] = useState(Array(data.length).fill(false));
+    const [selectedTagsArr, setSelectedTagsArr] = useState([]);
 
-  const toggleClass = (index, tag) => {
-    const newActiveStates = [...activeStates];
-    newActiveStates[index] = !newActiveStates[index];
-    setActiveStates(newActiveStates);
+    const toggleClass = (index, tag) => {
+        const newActiveStates = [...activeStates];
+        newActiveStates[index] = !newActiveStates[index];
+        setActiveStates(newActiveStates);
 
+        setSelectedTagsArr((prevState) => {
+            let response=[];
+            if (prevState.includes(tag)) {
+                response= prevState.filter(item => item !== tag);
+            } else {
+                response= [...prevState, tag];
+            }
+            onTagSelect(response)
+            return response;
+        })
+    };
 
-  if (selectedTags.includes(tag)) {
-      const updatedTags = selectedTags?.split(',')?.filter(t => t !== tag)?.join(',');
-      selectedTagsString(updatedTags)
-      setSelectedTags(updatedTags);
-    } else {
-      const updatedTags = selectedTags ? `${selectedTags}${tag}` : tag;
-      selectedTagsString(updatedTags)
-      setSelectedTags(updatedTags);
-    }
-  };
+    return (
 
-  return (
-
-    <div className={`d-flex align-items-center gap-2 flex-lg-wrap flex-nowrap ${classes.badgeBox} ${expolore && classes.explosreBox}`}>
-      {data.map((item, index) => (
-        <h5 key={index}>
-          <Badge
-            bg="secondary"
-            className={`${activeStates[index] ? "active p-3" : "p-3"} ${expolore ? `explore-badge` : ""}`}
-            onClick={() => toggleClass(index, item || item.title)}>
-            {item || item.title}
-          </Badge>
-        </h5>
-      ))}
-    </div>
-  );
+        <div
+            className={`d-flex align-items-center gap-2 flex-lg-wrap flex-nowrap ${classes.badgeBox} ${expolore && classes.explosreBox}`}>
+            {data.map((item, index) => (
+                <h5 key={index}>
+                    <Badge
+                        bg="secondary"
+                        className={`${activeStates[index] ? "active p-3" : "p-3"} ${expolore ? `explore-badge` : ""}`}
+                        onClick={() => toggleClass(index, (item || item.title).toLowerCase())}>
+                        {item || item.title}
+                    </Badge>
+                </h5>
+            ))}
+        </div>
+    );
 };
 
 export default AccordianBadge;
--- src/Pages/Explore/index.js
+++ src/Pages/Explore/index.js
@@ -1,71 +1,87 @@
 import AccordianBadge from "Components/AccordainBadge";
 import MemesDetails from "Components/Memes";
 import Search from "Components/Search";
-import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {Container} from "react-bootstrap";
 import postAPIs from "../../APIs/dashboard/home";
 import avatar from "../../Images/avatar.jpg";
 
 const Explore = () => {
-  const [recentPosts, setRecentPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [filteredPosts, setFilteredPost] = useState([]);
-  const [tags , setTags ] = useState([]);
+    const [recentPosts, setRecentPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [filteredPosts, setFilteredPost] = useState([]);
+    const [tags, setTags] = useState([]);
 
-  const getRecentPostAndTags = async () => {
-    try {
-      const res = await postAPIs.getRecentPosts();
-      if (res.status === 200) {
-        // Assuming a 200 status code means success
-        setRecentPosts(res.data.recent_posts);
-        setFilteredPost(res.data.recent_posts);
-        setLoading(false)
-      } else {
-        console.error("Error: Unexpected status code", res.status);
-      }
+    const getRecentPostAndTags = async () => {
+        try {
+            const res = await postAPIs.getRecentPosts();
+            if (res.status === 200) {
+                // Assuming a 200 status code means success
+                setRecentPosts(res.data.recent_posts);
+                setFilteredPost(res.data.recent_posts);
+                setLoading(false)
+            } else {
+                console.error("Error: Unexpected status code", res.status);
+            }
 
-      const resTags  = await postAPIs.getTags();
-      if(resTags){
-        setTags(resTags.data.tags);
-      }
-    } catch (error) {
-      console.error("Error while fetching data:", error);
+            const resTags = await postAPIs.getTags();
+            if (resTags) {
+                setTags(resTags.data.tags);
+            }
+        } catch (error) {
+            console.error("Error while fetching data:", error);
+        }
+    };
+
+
+    const onTagSearch = (value) => {
+        console.log("Value ", value)
+        if (value.length > 0) {
+            const filteredData = recentPosts.filter(item => {
+                const postTags = (item.post.tag_list || []).map(tag => tag.toLowerCase());
+                return value.some((searchTag) => postTags.some((postTag) => postTag.includes(searchTag)));
+            });
+            setFilteredPost(filteredData)
+        } else {
+            setFilteredPost(recentPosts);
+        }
+        /*  if (value.length > 0) {
+            console.log("Value ",value)
+            const filteredData = recentPosts.filter(recentPost => {
+              const usernameMatch = recentPost.username.toLowerCase().includes(value.toLowerCase());
+              const tagListMatch = recentPost.post.tag_list.some(tag => tag.toLowerCase().includes(value.toLowerCase()));
+              return usernameMatch || tagListMatch;
+            });
+
+            setFilteredPost(filteredData);
+          } else {
+            setFilteredPost(recentPosts);
+          }*/
     }
-  };
 
-
-  const onSearchChange = (value) => {
-
-    if (value.length > 0) {
-      
-      const filteredData = recentPosts.filter(user => {
-        const usernameMatch = user.username.toLowerCase().includes(value.toLowerCase());
-        const tagListMatch = user.post.tag_list.some(tag => tag.toLowerCase().includes(value.toLowerCase()));
-        return usernameMatch || tagListMatch;
-      });
-
-      setFilteredPost(filteredData);
-    } else {
-      setFilteredPost(recentPosts);
-      return;
+    const onTextSearch = (value) => {
+        if (value.length > 0) {
+            const filteredData = recentPosts.filter(recentPost => {
+                return recentPost.username.toLowerCase().includes(value.toLowerCase());
+            });
+            setFilteredPost(filteredData);
+        } else {
+            setFilteredPost(recentPosts);
+        }
     }
-  }
-
-
-  useEffect(() => {
-    getRecentPostAndTags();
-  }, []);
-  return (
-      <>
-        <section>
-          <Container fluid>
-            <Search expolore text={"Search hashtags, usernames"} onSearchChange={onSearchChange} />
-            <AccordianBadge data={tags} expolore selectedTagsString={onSearchChange} />
-            <MemesDetails isLoading={loading} newMemesData={filteredPosts} avatar={avatar} explore />
-          </Container>
-        </section>
-      </>
-  );
+    useEffect(() => {
+        getRecentPostAndTags();
+    }, []);
+    return (<>
+            <section>
+                <Container fluid>
+                    <Search expolore text={"Search hashtags, usernames"} onSearchChange={onTextSearch}/>
+                    <AccordianBadge data={tags} expolore
+                                    onTagSelect={onTagSearch}/>
+                    <MemesDetails isLoading={loading} newMemesData={filteredPosts} avatar={avatar} explore/>
+                </Container>
+            </section>
+        </>);
 };
 
 export default Explore;
