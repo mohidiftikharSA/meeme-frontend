480a3b9bec7f0a7ac93802934c8f4de2658d692c
Haseeb71 <71abdulhaseeb71@gmail.com>
2023-12-26 18:59:14 +0500

    Tournament Fixes

--- src/Pages/Judge/index.js
+++ src/Pages/Judge/index.js
@@ -1,12 +1,12 @@
 import Heading from "Components/Heading";
-import React, {useEffect, useState} from "react";
-import {Button, Container} from "react-bootstrap";
-import {useLocation} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Button, Container } from "react-bootstrap";
+import { useLocation } from 'react-router-dom';
 import judge1 from "../../Images/judge1.png";
 import judge2 from "../../Images/judge2.png";
 import classes from "./index.module.scss";
-import {AiFillHeart} from 'react-icons/ai';
-import {FaTimes} from 'react-icons/fa';
+import { AiFillHeart } from 'react-icons/ai';
+import { FaTimes } from 'react-icons/fa';
 import api from 'APIs/tournaments'
 import SkeletonTournamentPostsLoading from "../../Components/Loader/SkeletonTournamentPostsLoading";
 import Skeleton from "react-loading-skeleton";
@@ -31,7 +31,7 @@ const JudgePage = () => {
     }
     const likeDislikePost = async (post_id, isLike = true) => {
         console.log("Post ID ", post_id)
-        const response = isLike ? await api.likeTournamentPost({post_id}) : await api.disLikeTournamentPost({post_id});
+        const response = isLike ? await api.likeTournamentPost({ post_id }) : await api.disLikeTournamentPost({ post_id });
         if (response.status == 200) {
             removePostFromList(post_id)
             setImagesLoaded([])
@@ -62,45 +62,54 @@ const JudgePage = () => {
         setImagesLoaded([]);
         setLikedPostsCount(new URLSearchParams(location.search).get('count') ?? 0);
     }, []);
-    return (<section>
-        <Container>
-            <div className="sectionHolder">
-                <Heading title={"Judge"} judge linkPath={"tornament"} likedCounts={likedPostsCount}/>
-                {isLoading ? <SkeletonTournamentPostsLoading/> : tournamentPosts.map((item, ind) => {
-                    return (<div
-                        key={`${item.id}_t_posts_${ind}`}
-                        className={classes.postHolder}>
-                        <div style={{display: imagesLoaded[ind] ? 'none' : 'block'}}>
-                            <Skeleton
-                                height={300} width="100%"
-                                style={{
-                                    marginTop: '10px', borderRadius: '20px'
-                                }}/>
-                        </div>
-                        <div className="imgBox">
-                            <img
-                                src={item.post_image}
-                                alt="icon"
-                                style={{borderRadius: "30px", display: imagesLoaded[ind] ? 'block' : 'none'}}
-                                onLoad={() => handleImageLoad(ind)}
-                                onError={() => handleImageError(ind)}
-                            />
-                        </div>
-                        <div className={classes.btnGroup}>
-                            <Button onClick={() => {
-                                likeDislikePost(item.id)
-                            }}><AiFillHeart/></Button>
 
-                            <Button onClick={() => {
-                                likeDislikePost(item.id, false)
-                            }}><FaTimes/></Button>
-                        </div>
-                    </div>);
-                })}
 
-            </div>
-        </Container>
-    </section>);
+    return (
+        <>
+            <section>
+                <Container>
+                    <div className="sectionHolder">
+                        <Heading title={"Judge"} judge linkPath={"tornament"} likedCounts={likedPostsCount} />
+                        {isLoading ? <SkeletonTournamentPostsLoading /> : tournamentPosts.length === 0 ? (
+                            <p style={{
+                                marginTop: '10px', textAlign: 'center'
+                            }}> There's no Tournament Posts to Show</p>
+                        ) : tournamentPosts.map((item, ind) => {
+                            return (<div
+                                key={`${item.id}_t_posts_${ind}`}
+                                className={classes.postHolder}>
+                                <div style={{ display: imagesLoaded[ind] ? 'none' : 'block' }}>
+                                    <Skeleton
+                                        height={300} width="100%"
+                                        style={{
+                                            marginTop: '10px', borderRadius: '20px'
+                                        }} />
+                                </div>
+                                <div className="imgBox">
+                                    <img
+                                        src={item.post_image}
+                                        alt="icon"
+                                        style={{ borderRadius: "30px", display: imagesLoaded[ind] ? 'block' : 'none' }}
+                                        onLoad={() => handleImageLoad(ind)}
+                                        onError={() => handleImageError(ind)}
+                                    />
+                                </div>
+                                <div className={classes.btnGroup}>
+                                    <Button onClick={() => {
+                                        likeDislikePost(item.id)
+                                    }}><AiFillHeart /></Button>
+
+                                    <Button onClick={() => {
+                                        likeDislikePost(item.id, false)
+                                    }}><FaTimes /></Button>
+                                </div>
+                            </div>);
+                        })}
+                    </div>
+                </Container>
+            </section>
+        </>
+    );
 };
 
 export default JudgePage;
