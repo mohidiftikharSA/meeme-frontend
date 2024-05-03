ba07547f260c440c89b1e153bc68ea0e1a89d711
Haseeb71 <71abdulhaseeb71@gmail.com>
2024-05-03 15:49:38 +0500

    Trying fixes

--- src/Components/ChatPopup/index.js
+++ src/Components/ChatPopup/index.js
@@ -1,230 +1,168 @@
 import React, { useEffect, useRef, useState } from "react";
-import MessagesAPIs from '../../APIs/messages';
+import MessagesAPIs from "../../APIs/messages";
 import { useSelector } from "react-redux";
-// import { useActionCable, useChannel } from '@aersoftware/react-use-action-cable';
+import { ActionCable,ActionCableProvider } from "react-actioncable-provider";
 import ChatDropdown from "./ChatDropdown";
 import ChatWindow from "./ChatWindow";
 
-
 const ChatPopup = ({ isOpen, onClose, profile, data }) => {
-    const [inputText, setInputText] = useState("");
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 992);
-    const [isChatVisible, setIsChatVisible] = useState(false);
-    const [inboxList, setInboxList] = useState([]);
-    const [selectedChat, setSelectedChat] = useState();
-    const [msgsList, setMsgsList] = useState([]);
-    const { user, accessToken } = useSelector((state) => state.auth);
-    const [imgForAPI, setImgForAPI] = useState(null);
-    const fileInputRef = useRef(null);
-    const [msgSent, setMsgSent] = useState();
+  const [inputText, setInputText] = useState("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 992);
+  const [isChatVisible, setIsChatVisible] = useState(false);
+  const [inboxList, setInboxList] = useState([]);
+  const [selectedChat, setSelectedChat] = useState();
+  const [msgsList, setMsgsList] = useState([]);
+  const { user, accessToken } = useSelector((state) => state.auth);
+  const [imgForAPI, setImgForAPI] = useState(null);
+  const fileInputRef = useRef(null);
+  const [msgSent, setMsgSent] = useState();
 
+  useEffect(() => {
+    setIsChatVisible(isOpen);
 
-    // const { actionCable } = useActionCable(`${process.env.REACT_APP_WS_URL ?? 'ws://localhost:3000'}/cable?token=${accessToken}`);
-    // const { subscribe, unsubscribe, send } = useChannel(actionCable)
-
-
-    useEffect(() => {
-        setIsChatVisible(isOpen);
-
-        /**
-         *   To Do
-         */
-
-        // const chat = inboxList.filter(item => item.sender_id === profile?.user?.id || item.receiver_id === profile?.user?.id);
-        // console.log("My chat filter  === ", chat);
-        // if (chat[0] && chat) {
-        //   setSelectedChat(chat);
-        // }
-
-
-        if (data && profile) {
-            const obj = {
-                sender_id: data?.user?.id,
-                conversation_id: null,
-                receiver_image: data?.user_image,
-                sender_name: data?.user?.username
-            }
-            setSelectedChat(obj);
-        }
-
-    }, [isOpen, profile, data]);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsWideScreen(window.innerWidth >= 768);
-        };
-        window.addEventListener("resize", handleResize);
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
-
-    useEffect(() => {
-        getInboxList();
-    }, [])
-
-    useEffect(() => {
-        getInboxList();
-    }, [msgSent])
-
-
-    useEffect(() => {
-
-        if (selectedChat) {
-            if (user.id === selectedChat.sender_id) {
-                getChatMessages(selectedChat.receiver_id)
-            } else {
-                getChatMessages(selectedChat.sender_id)
-            }
-        }
-
-    }, [selectedChat]);
-
-    const getChatMessages = async (receiverId) => {
-        if (receiverId) {
-            const msgs = await MessagesAPIs.getChatMessages(receiverId);
-            if (msgs) {
-                setMsgsList(msgs.data.messages.reverse());
-            }
-        }
+    if (data && profile) {
+      const obj = {
+        sender_id: data?.user?.id,
+        conversation_id: null,
+        receiver_image: data?.user_image,
+        sender_name: data?.user?.username,
+      };
+      setSelectedChat(obj);
     }
+  }, [isOpen, profile, data]);
 
-    const getInboxList = async () => {
-        const res = await MessagesAPIs.getInboxList();
-        if (res) {
-            setInboxList(res.data.messages);
-        }
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(window.innerWidth >= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
+  useEffect(() => {
+    getInboxList();
+  }, []);
+
+  useEffect(() => {
+    getInboxList();
+  }, [msgSent]);
+
+  useEffect(() => {
+    if (selectedChat) {
+      if (user.id === selectedChat.sender_id) {
+        getChatMessages(selectedChat.receiver_id);
+      } else {
+        getChatMessages(selectedChat.sender_id);
+      }
     }
+  }, [selectedChat]);
 
-    const handleInputChange = (e) => {
-        setInputText(e.target.value);
-    };
-
-    const sendMessage = async () => {
-        if (!selectedChat.conversation_id) {
-            var res = await MessagesAPIs.createConversation({ receiver_id: selectedChat?.sender_id })
-            if (res) {
-                const resObj = { ...selectedChat };
-                resObj['conversation_id'] = res.data?.conversation?.id
-                console.log("New Response obj of Conversation == ", resObj);
-                setSelectedChat(resObj);
-            }
-        }
-        if (inputText.trim() === "") return;
-        const data = new FormData();
-        data.append('conversation_id', selectedChat.conversation_id || res.data?.conversation?.id);
-        data.append('receiver_id', selectedChat.sender_id === user.id ? selectedChat.receiver_id : selectedChat.sender_id);
-        data.append('body', inputText);
-        if (imgForAPI) {
-            data.append("message_images[]", imgForAPI);
-        }
-        const sendMsg = await MessagesAPIs.sendMessage(data);
-        if (sendMsg) {
-            setMsgSent(sendMsg.data);
-        }
-        const newMessage = {
-            text: inputText, user: user,
-        };
-        setImgForAPI(null);
-        setInputText("");
-    };
-
-
-    const handleClick = (chat) => {
-        setIsChatVisible(true);
-        setSelectedChat(chat);
-    };
-    const chatToggle = () => {
-        setIsChatVisible(false);
-    };
-
-    const toggleDropdown = () => {
-        setIsDropdownOpen(!isDropdownOpen);
-    };
-
-    /**
-     * Chat Socket Implementation.
-     */
-
-    useEffect(() => {
-        // if (selectedChat) {
-        //     try {
-        //         subscribe({
-        //             channel: `ConversationsChannel`,
-        //             channel_key: `conversation_${selectedChat.conversation_id}`,
-        //             conversation_id: selectedChat.conversation_id,
-        //         }, {
-        //             received: (msg) => {
-        //                 console.log("Recived Messages from Socket - ", msg.body);
-        //                 console.log("Msgs List === ", msgsList);
-        //                 setMsgsList((prevState) => ([...prevState, msg.body]));
-
-        //             }, connected: () => {
-        //                 console.log('Socket Connected Successfully');
-        //             },
-        //         },);
-        //     } catch (err) {
-        //         console.log('err', err);
-        //     }
-
-        //     return () => {
-        //         unsubscribe();
-        //     };
-        // }
-    }, [selectedChat]);
-
-    const handleFileChange = (e) => {
-        const file = e.target.files[0];
-        if (file) {
-            setImgForAPI(e.target.files[0])
-        }
-    };
-
-    const handleImageUpload = () => {
-        fileInputRef.current.click();
-    };
-
-    const handleFileInput = (e) => {
-        const file = e.target.files[0];
-        if (file) {
-            console.log("Selected File ==", file);
-            setImgForAPI(e.target.files[0])
-        }
-    };
-
-    const handleKeyPress = (event) => {
-        if (event.key === "Enter") {
-            sendMessage();
-        }
-    };
-
-    function countOnlineUsers() {
-        const trueCount = inboxList.reduce((count, item) => {
-            if (item.receiver_active_status === true && item?.receiver_id !== user?.id) {
-                return count + 1;
-            } else if (item.sender_active_status === true && item?.sender_id !== user?.id) {
-                return count + 1;
-            }
-            return count;
-        }, 0);
-
-        return trueCount;
+  const getChatMessages = async (receiverId) => {
+    if (receiverId) {
+      const msgs = await MessagesAPIs.getChatMessages(receiverId);
+      if (msgs) {
+        setMsgsList(msgs.data.messages.reverse());
+      }
     }
+  };
 
-    return (<>
+  const getInboxList = async () => {
+    const res = await MessagesAPIs.getInboxList();
+    if (res) {
+      setInboxList(res.data.messages);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const sendMessage = async () => {
+    // Your existing sendMessage logic
+  };
+
+  const handleClick = (chat) => {
+    setIsChatVisible(true);
+    setSelectedChat(chat);
+  };
+
+  const chatToggle = () => {
+    setIsChatVisible(false);
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleReceivedMessage = (response) => {
+    // Process received messages here
+    console.log("Received message:", response);
+    setMsgsList((prevState) => [...prevState, response.body]);
+  };
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImgForAPI(e.target.files[0]);
+    }
+  };
+
+  const handleImageUpload = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileInput = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      console.log("Selected File ==", file);
+      setImgForAPI(e.target.files[0]);
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      sendMessage();
+    }
+  };
+
+  function countOnlineUsers() {
+    const trueCount = inboxList.reduce((count, item) => {
+      if (
+        item.receiver_active_status === true &&
+        item?.receiver_id !== user?.id
+      ) {
+        return count + 1;
+      } else if (
+        item.sender_active_status === true &&
+        item?.sender_id !== user?.id
+      ) {
+        return count + 1;
+      }
+      return count;
+    }, 0);
+
+    return trueCount;
+  }
+
+  return (
+    <>
+      <ActionCableProvider
+        url={`wss://1f9b-139-135-32-227.ngrok-free.app/cable?token=${accessToken}`}
+      >
         <ChatDropdown
-            isDropdownOpen={isDropdownOpen}
-            toggleDropdown={toggleDropdown}
-            countOnlineUsers={countOnlineUsers}
-            isWideScreen={isWideScreen}
-            isOpen={isOpen}
-            inboxList={inboxList}
-            handleClick={handleClick}
-            user={user}
+          isDropdownOpen={isDropdownOpen}
+          toggleDropdown={toggleDropdown}
+          countOnlineUsers={countOnlineUsers}
+          isWideScreen={isWideScreen}
+          isOpen={isOpen}
+          inboxList={inboxList}
+          handleClick={handleClick}
+          user={user}
         />
-        {isChatVisible && (<ChatWindow
+        {isChatVisible && (
+          <ChatWindow
             isChatVisible={isChatVisible}
             chatToggle={chatToggle}
             selectedChat={selectedChat}
@@ -238,8 +176,22 @@ const ChatPopup = ({ isOpen, onClose, profile, data }) => {
             handleFileInput={handleFileInput}
             handleFileChange={handleFileChange}
             fileInputRef={fileInputRef}
-        />)}
-    </>);
+          />
+        )}
+        {/* ActionCable component from react-actioncable-provider */}
+        {selectedChat && (
+          <ActionCable
+            key={selectedChat.conversation_id}
+            channel={{
+              channel: "ConversationsChannel",
+              conversation_id: selectedChat.conversation_id,
+            }}
+            onReceived={handleReceivedMessage}
+          />
+        )}
+      </ActionCableProvider>
+    </>
+  );
 };
 
 export default ChatPopup;
--- src/Containers/App/index.js
+++ src/Containers/App/index.js
@@ -1,11 +1,19 @@
 import React, { useEffect } from "react";
 import ClientRoutes from "Routes";
+import { ActionCableProvider } from "react-actioncable-provider";
 import "Style/global.scss";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const { accessToken } = useSelector((state) => state.auth);
+
   return (
     <>
+      {/* <ActionCableProvider
+        url={`wss://1f9b-139-135-32-227.ngrok-free.app/cable?token=${accessToken}`}
+      > */}
       <ClientRoutes />
+      {/* </ActionCableProvider> */}
     </>
   );
 };
