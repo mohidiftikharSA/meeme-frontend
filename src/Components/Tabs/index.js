24680246829c20239c2607d3d1add0e25dba3c1b
Haseeb71 <71abdulhaseeb71@gmail.com>
2024-09-16 14:19:52 +0500

    faq changes

--- src/Components/Tabs/index.js
+++ src/Components/Tabs/index.js
@@ -461,26 +461,6 @@ const TabDetails = ({
                 Memee accepts variety of payment methods which includes PayPal,
                 Bitcoin, Bank trasnfers, Credit/Debit Cards, Google Pay, Apple
               </p>
-              <p className="faqSubTitle">
-                {" "}
-                How do I place a cancellation request?
-              </p>
-              <p>
-                Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et.
-                Sunt qui esse pariatur duis deserunt mollit dolore cillum minim
-                tempor enim. Elit aute irure tempor cupidatat incididunt sint
-                deserunt ut voluptate aute id deserunt nisi. Aliqua id fugiat
-                nostrud irure ex duis ea quis id quis ad et. Sunt qui esse
-                pariatur duis deserunt mollit dolore cillum minim tempor enim.
-                Elit aute irure tempor cupidatat incididunt sint deserunt ut
-                voluptate aute id deserunt nisi.
-              </p>
-              <p className="faqSubTitle">How do I edit or remove a method? </p>
-              <p>
-                Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et.
-                Sunt qui esse pariatur duis deserunt mollit dolore cillum minim
-                tempor enim.
-              </p>
             </div>
           </Tab>
         </Tabs>
